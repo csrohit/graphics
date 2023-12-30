1ed39c294df233cb89b05532f57de4dfc405d740
Rohit Nimkar <nehalnimkar@gmail.com>
2023-12-30 20:36:47 +0530

    create logical device

--- vulkan/01-Triangle/myheader.cpp
+++ vulkan/01-Triangle/myheader.cpp
@@ -14,6 +14,7 @@ void        DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMesse
 void        populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT &createInfo);
 static void pickPhysicalDevice();
 static void setupDebugMessenger();
+static void createLogicalDevice();
 static QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device);
 #ifdef NDEBUG
 const bool enableValidationLayers = false;
@@ -27,6 +28,8 @@ VkInstance                      instance         = {};
 const std::vector<const char *> validationLayers = {"VK_LAYER_KHRONOS_validation"};
 VkPhysicalDevice                physicalDevice   = VK_NULL_HANDLE;
 VkDebugUtilsMessengerEXT        debugMessenger;
+VkDevice                        device;
+VkQueue                         graphicsQueue;
 
 static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT      messageSeverity,
                                                     VkDebugUtilsMessageTypeFlagsEXT             messageType,
@@ -46,6 +49,7 @@ int initVulkan()
     }
     setupDebugMessenger();
     pickPhysicalDevice();
+    createLogicalDevice();
 
     return (0);
 }
@@ -122,6 +126,11 @@ void setupDebugMessenger()
 }
 void cleanUp()
 {
+    if (nullptr == device)
+    {
+        vkDestroyDevice(device, nullptr);
+    }
+
     if (enableValidationLayers)
     {
         DestroyDebugUtilsMessengerEXT(instance, debugMessenger, nullptr);
@@ -252,6 +261,48 @@ QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device)
     }
 
     return indices;
+}
+
+void createLogicalDevice()
+{
+    QueueFamilyIndices indices = findQueueFamilies(physicalDevice);
+
+    VkDeviceQueueCreateInfo queueCreateInfo{};
+    queueCreateInfo.sType            = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
+    queueCreateInfo.queueFamilyIndex = indices.graphicsFamily.value();
+    queueCreateInfo.queueCount       = 1;
+
+    float queuePriority              = 1.0f;
+    queueCreateInfo.pQueuePriorities = &queuePriority;
+
+    VkPhysicalDeviceFeatures deviceFeatures{};
+
+    VkDeviceCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+
+    createInfo.pQueueCreateInfos    = &queueCreateInfo;
+    createInfo.queueCreateInfoCount = 1;
+
+    createInfo.pEnabledFeatures = &deviceFeatures;
+
+    createInfo.enabledExtensionCount = 0;
+
+    if (enableValidationLayers)
+    {
+        createInfo.enabledLayerCount   = static_cast<uint32_t>(validationLayers.size());
+        createInfo.ppEnabledLayerNames = validationLayers.data();
+    }
+    else
+    {
+        createInfo.enabledLayerCount = 0;
+    }
+
+    if (vkCreateDevice(physicalDevice, &createInfo, nullptr, &device) != VK_SUCCESS)
+    {
+        throw std::runtime_error("failed to create logical device!");
+    }
+
+    vkGetDeviceQueue(device, indices.graphicsFamily.value(), 0, &graphicsQueue);
 }
 
 /*--- temp ---*/
